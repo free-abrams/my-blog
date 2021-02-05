9743f43aa2d7195567ff72bc98aec39ebd93f8c6
alex78218 <sa130494625@qq.com>
2021-02-05 18:12:05 +0800

    'migration'

---
+++ database/migrations/2021_02_05_100454_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->integer('parent_id')->index()->comment('父级id');
+            $table->integer('level')->comment('层级');
+            $table->text('path')->comment('路径');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
